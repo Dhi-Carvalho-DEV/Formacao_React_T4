86e3a0d049abcd1b1a3ebced6f19bfcbb68cd4a6
Diogo Santana <102337705+Dhi-Carvalho-DEV@users.noreply.github.com>
2023-05-05 05:39:01 -0300

    Refatorando o código do projeto

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,27 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import Banner from './components/Banner/Banner';
+import Banner from './components/Banner/Banner'
 
 function App() {
   return (
     <div className="App">
-      <Banner/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Olá, Mundo!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
