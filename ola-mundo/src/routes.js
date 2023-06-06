f6cc17d50f9e51b65f4a1216be2b649a7840b3ea
Diogo Santana <102337705+Dhi-Carvalho-DEV@users.noreply.github.com>
2023-06-05 23:54:15 -0300

    Finalização da seção Sobre Mim

--- ola-mundo/src/routes.js
+++ ola-mundo/src/routes.js
@@ -3,14 +3,17 @@ import Inicio from "./pages/Inicio";
 import SobreMim from "./pages/SobreMim";
 import Menu from "./components/Menu";
 import Rodape from "components/Rodape";
+import PaginaPadrao from "components/PaginaPadrao.js";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Menu />
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route index element={<Inicio />} />
+          <Route path="sobremim" element={<SobreMim />} />
+        </Route>
         <Route path="*" element={<div>Página não encontrada</div>} />
       </Routes>
       <Rodape />
